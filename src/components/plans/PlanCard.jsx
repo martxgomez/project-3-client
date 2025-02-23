d1f2b8a1066446c6541cd1f8dc447a9dc5068258
martxgomez <martagomezmartinez95@gmail.com>
2025-02-23 17:49:49 +0100

    date formatted

--- src/components/plans/PlanCard.jsx
+++ src/components/plans/PlanCard.jsx
@@ -1,9 +1,14 @@
 import { Link } from "react-router-dom";
 
-function PlanCard({ plan}) {
-  const { _id, title, date, location, image } = plan
+function PlanCard({ plan }) {
+  const { _id, title, date, location, image } = plan;
   console.log(plan);
-  
+
+  const formatDate = (prevDate) => {
+    const date = new Date(prevDate);
+    return date.toLocaleDateString("es-ES");
+  };
+
   return (
     <article>
       <Link to={`/plans/${_id}`} className="card">
@@ -12,7 +17,7 @@ function PlanCard({ plan}) {
           <div>
             <h2>{title}</h2>
             <p>{location}</p>
-            <p>{date}</p>
+            <p>{formatDate(date)}</p>
           </div>
         </div>
       </Link>
