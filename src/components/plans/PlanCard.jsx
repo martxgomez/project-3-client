ea9f1106d09b7b180a520ea4b95edd30d570e6aa
Tomás Martín <tmartindsgn@gmail.com>
2025-03-05 19:42:15 +0100

    merge conflict fixed

--- src/components/plans/PlanCard.jsx
+++ src/components/plans/PlanCard.jsx
@@ -1,6 +1,10 @@
 import { Link } from "react-router-dom";
 import "./PlanCard.css";
 import calendar from "../../assets/calendar.svg"
+import { useState, useContext } from "react"
+import { UserContext } from "../../context/UserContext";
+
+
 
 
 function PlanCard({ plan, formatDate }) {
