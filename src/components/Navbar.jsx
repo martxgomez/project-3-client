e537fb986cfdd79444424410eb7cd4041c7c7289
martxgomez <martagomezmartinez95@gmail.com>
2025-03-07 18:44:33 +0100

    logo userhomepage

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,25 +7,28 @@ import { useContext } from "react";
 import { UserContext } from "../context/UserContext";
 import Logo from "../assets/logo_ok.png";
 
-import "./Navbar.css"
-
+import "./Navbar.css";
 
 //COMPONENTS
 import Menubutton from "./MenuButton";
 
-function Navbar({ onClick, iconSource })  {
-  const {user} = useContext(UserContext)
+function Navbar({ onClick, iconSource }) {
+  const { user } = useContext(UserContext);
   if (!user) {
     return (
       <div className="nav-bar">
-        <NavLink to="/"><img className="nav-bar__logo" src={Logo} alt="Logo prueba" /></NavLink>
+        <NavLink to="/">
+          <img className="nav-bar__logo" src={Logo} alt="Logo prueba" />
+        </NavLink>
         <NavLink to="/log-in">
           {" "}
           <button className="nav-bar__iniciar-button"> Inicia sesión</button>
         </NavLink>
         <NavLink to="/sign-up">
           {" "}
-          <button className="nav-bar__registrate-button">Registrate</button>{" "}
+          <button className="nav-bar__registrate-button">
+            Registrate
+          </button>{" "}
         </NavLink>
       </div>
     );
@@ -33,7 +36,9 @@ function Navbar({ onClick, iconSource })  {
     return (
       <div className="nav-bar">
         <Menubutton onClick={onClick} iconSource={iconSource} />
-        <img className="nav-bar__logo" src={Logo} alt="Logo prueba" />
+        <NavLink to="/user-homepage/">
+          <img className="nav-bar__logo" src={Logo} alt="Logo prueba" />
+        </NavLink>
       </div>
     );
   }
